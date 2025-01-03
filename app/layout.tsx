7fe73b07191efbd5e3e06997081a1cb9fc4e4b5e
Takuma Okada <okayan0128@hotmail.com>
2025-01-04 00:59:35 +0900

    不要なもの整理

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://t-okada-portfolio.vercel.app/"),
+  title: {
+    template: "T.OKADA's PORTFOLIO",
+    default: "T.OKADAのポートフォリオサイトです。",
+  },
+};
 
 export default function RootLayout({
   children,
