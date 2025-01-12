191d0e854d116a9d4bb6270328ec7d35eba09b49
Takuma Okada <okayan0128@hotmail.com>
2025-01-12 23:29:02 +0900

    ボタンの不具合修正

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,28 +1,29 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./index.module.css";
 import React, { useState, useEffect, useRef } from "react";
+import styles from "./index.module.css";
 
 export default function Header() {
-  const [openMenu, setOpenMenu] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false); // メニュー開閉状態
   const menuRef = useRef<HTMLDivElement | null>(null); // メニュー領域の参照
+  const buttonRef = useRef<HTMLButtonElement | null>(null); // ボタンの参照
 
-  const handleMenuOpen = () => {
-    setOpenMenu(!openMenu);
-  };
-  const handleLinkClick = () => {
-    setOpenMenu(false);
+  // メニューの開閉をトグル
+  const handleMenuToggle = () => {
+    setOpenMenu((prev) => !prev); // 開閉をトグル
   };
 
-  // メニュー外クリックを監視するuseEffect
+  // メニュー外クリックの監視
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
         menuRef.current &&
-        !menuRef.current.contains(event.target as Node) // 型キャストで Node として扱う
+        !menuRef.current.contains(event.target as Node) && // メニュー外かどうか
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node) // ボタン外かどうか
       ) {
-        setOpenMenu(false);
+        setOpenMenu(false); // メニューを閉じる
       }
     };
 
@@ -37,64 +38,67 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
+      {/* ロゴ */}
       <h1>
-        <a href="/" className={styles.logoLink}>
+        <Link href="/" className={styles.logoLink}>
           <Image
             className={styles.logoImg}
             src="/logo.png"
             alt="T.OKADA's PORTFOLIO"
             width={517}
             height={50}
+            priority // LCPエラー解消
           />
-        </a>
+        </Link>
       </h1>
-      <button onClick={handleMenuOpen} type="button" className="z-10 space-y-2">
+
+      {/* ハンバーガーメニューアイコン */}
+      <button
+        ref={buttonRef} // ボタン領域の参照を設定
+        onClick={handleMenuToggle}
+        type="button"
+        className="z-10 space-y-2"
+      >
         <div
-          className={
-            openMenu
-              ? "w-8 h-0.5 bg-gray-600 translate-y-2.5 rotate-45 transition duration-500 ease-in-out"
-              : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
-          }
+          className={`w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out ${
+            openMenu ? "rotate-45 translate-y-2.5" : ""
+          }`}
         />
         <div
-          className={
-            openMenu
-              ? "opacity-0 transition duration-500 ease-in-out"
-              : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
-          }
+          className={`w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out ${
+            openMenu ? "opacity-0" : ""
+          }`}
         />
         <div
-          className={
-            openMenu
-              ? "w-8 h-0.5 bg-gray-600 -rotate-45 transition duration-500 ease-in-out"
-              : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
-          }
+          className={`w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out ${
+            openMenu ? "-rotate-45 -translate-y-2.5" : ""
+          }`}
         />
       </button>
+
+      {/* メニュー */}
       <nav
-        ref={menuRef} // メニュー領域に参照を設定
-        className={
-          openMenu
-            ? "text-left fixed bg-slate-50 right-0 top-0 w-8/12 h-screen flex flex-col justify-start pt-8 px-3 ease-linear duration-300"
-            : "fixed right-[-100%] ease-linear duration-300"
-        }
+        ref={menuRef} // メニュー領域の参照
+        className={`fixed right-0 top-0 w-8/12 h-screen bg-slate-50 flex flex-col justify-start pt-8 px-3 ease-linear duration-200 ${
+          openMenu ? "translate-x-0" : "translate-x-full"
+        }`}
       >
         <ul className="mt-6">
-          <li className="">
+          <li>
             <Link
               href="/"
               className="py-2 inline-block"
-              onClick={handleLinkClick}
+              onClick={() => setOpenMenu(false)} // クリックでメニューを閉じる
             >
               <span className="text-3xl font-bold text-main-color">HOME</span>
               <p className="text-base">トップページ</p>
             </Link>
           </li>
-          <li className="">
+          <li>
             <Link
               href="/profile"
               className="py-2 inline-block"
-              onClick={handleLinkClick}
+              onClick={() => setOpenMenu(false)} // クリックでメニューを閉じる
             >
               <span className="text-3xl font-bold text-main-color">
                 PROFILE
@@ -102,11 +106,11 @@ export default function Header() {
               <p className="text-base">プロフィール</p>
             </Link>
           </li>
-          <li className="">
+          <li>
             <Link
               href="/portfolio"
               className="py-2 inline-block"
-              onClick={handleLinkClick}
+              onClick={() => setOpenMenu(false)} // クリックでメニューを閉じる
             >
               <span className="text-3xl font-bold text-main-color">
                 PORTFOLIO
